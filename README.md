474f73d0e30efe5ed6d361c5fc1d2e55c425c183
karim fanti <karimwebdevelopment@gmail.com>
2021-04-11 17:51:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - Bookmark landing page solution
+
+This is a solution to the [Bookmark landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/bookmark-landing-page-5d0b588a9edda32581d29158). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the newsletter form is submitted if:
+  - The input field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS preprocessor
+- CSS custom properties
+- CSS pseudo elements
+- CSS pseudo classes
+- Flexbox
+- CSS Grid
+- Javascript Functional
+- Mobile-first workflow
+
+### What I learned
+
+In this challenge, I finally learned CSS pseudo-classes and pseudo-elements. I struggled with them for a long time, and in this challenge, I used them in nearly every section. For example, in the form section, the error alert is all made using pseudo-elements, the error icon is a ::before element, and the message below the input is an ::after element. Another example is the background behind the illustrations. They are made using ::after because they need to expand through the entire screen, which couldn't be done using background-image.
+
+## Author
+
+- Website - SOON
+- Frontend Mentor - [@Galielo-App](https://www.frontendmentor.io/profile/Galielo-App)
---
+++ src/script/app.js
@@ -0,0 +1,86 @@
+const mobileToggler = document.getElementById('mobileToggler');
+const sliderMenu = document.getElementById('sliderMenu');
+const form = document.querySelector('.contact__form');
+const faqs = document.querySelector('.faqs__questions');
+
+sliderMenu.addEventListener('click', sliderChangeMenu);
+mobileToggler.addEventListener('click', toggleMenu);
+form.addEventListener('submit', validateEmail);
+faqs.addEventListener('click', showFaq);
+
+//MENU AREA
+function toggleMenu() {
+  const navigationMenu = document.getElementById('navigationMenu');
+  const overlay = document.getElementById('overlay');
+  const headerLogo = document.getElementById('headerLogo');
+  const body = document.querySelector('body');
+  const html = document.querySelector('html');
+
+  mobileToggler.classList.toggle('header__mobile-menu--active');
+  navigationMenu.classList.toggle('header__navigation--active');
+  overlay.classList.toggle('header__overlay--active');
+  headerLogo.classList.toggle('header__logo--white');
+  body.classList.toggle('dont-overflow');
+  html.classList.toggle('dont-overflow');
+}
+
+//SLIDER AREA
+function sliderChangeMenu(event) {
+  const target = event.target;
+  const items = document.querySelectorAll('.features__slider-item');
+
+  items.forEach(item => item.classList.remove('--active'));
+  target.classList.add('--active');
+  displaySliderContent(target);
+}
+
+function displaySliderContent(target) {
+  const sliderTitle = document.getElementById('slider-title');
+  const sliderParagraph = document.getElementById('slider-paragraph');
+  const sliderIllustration = document.getElementById('slider-illustration');
+
+  if (target.classList.contains('first-button')) {
+    sliderTitle.textContent = 'Bookmark in one click';
+    sliderParagraph.textContent = 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.';
+    sliderIllustration.src = '../public/images/illustration-features-tab-1.svg';
+  } else if (target.classList.contains('second-button')) {
+    sliderTitle.textContent = 'Intelligent Search';
+    sliderParagraph.textContent = 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.';
+    sliderIllustration.src = '../public/images/illustration-features-tab-2.svg';
+  } else if (target.classList.contains('third-button')) {
+    sliderTitle.textContent = 'Share your bookmarks';
+    sliderParagraph.textContent = 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.';
+    sliderIllustration.src = '../public/images/illustration-features-tab-3.svg';
+  }
+}
+
+//FORM AREA
+function validateEmail(event) {
+  const input = document.querySelector('.contact__input-label');
+  const userEmail = document.querySelector('.contact__email-input').value;
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  userEmail.match(regex) ? sendEmail(input) : showError(event, input);
+}
+
+function showError(event, input) {
+  event.preventDefault();
+  input.classList.add('show-error');
+}
+
+function sendEmail(input) {
+  input.classList.remove('show-error');
+}
+
+//FAQS AREA
+function showFaq(event) {
+  const questions = document.querySelectorAll('.faqs__question');
+  const question = event.target;
+
+  if (question.parentElement.classList.contains('show')) {
+    question.parentElement.classList.remove('show');
+  } else if (question.classList.contains('top-area')) {
+    questions.forEach(element => element.classList.remove('show'));
+    question.parentElement.classList.toggle('show');
+  }
+}
